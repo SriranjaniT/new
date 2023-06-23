44ca4f2f88993df010345fc4cef9028168a28d36
SriranjaniT <sriranjanit9@gmail.com>
2023-06-23 10:18:38 +0530

    new

--- src/pro14/Main1.java
+++ src/pro14/Main1.java
@@ -5,8 +5,8 @@ public class Main1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
          double base=2.5;
-         int expo=2;
-         double res=3.0;
+         int expo=7;
+         double res=4.0;
          
          while(expo!=0) 
          {
